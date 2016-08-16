f2e5d3b7a768d0fa82b1393d625592dfcd44b5e5
Tony Brown <tony@tonybrown.me>
2016-08-16 02:50:21 -0400

    started lessons

---
+++ js/clientApp.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var div = React.DOM.div
+var h1  = React.DOM.h1
+
+var MyFirstComponent = (
+	div(null,
+		h1(null, 'This is my first Component!')
+	)
+)
+
+ReactDOM.render(
+	MyFirstComponent, document.getElementById('app')
+)
