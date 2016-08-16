e6815a0cf9b87ee92805201597acaa059c1d7236
Tony Brown <tony@tonybrown.me>
2016-08-16 04:39:42 -0400

    add color prop

--- js/clientApp.js
+++ js/clientApp.js
@@ -1,31 +1,30 @@
 'use strict';
 
-var div = React.DOM.div
-var h1  = React.DOM.h1
-
+var div = React.DOM.div;
+var h1  = React.DOM.h1;
 // a composite component
 var MyTitle = React.createClass({
 	render () {
 		return (
 			div(null,
-				h1(null, this.props.title)
-				//h1(null, 'Checkout this other title')
+				h1({style: {color: this.props.color}}, this.props.title)
 			)
-		)
+		);
 	}
-})
+});
 
 var MyTitleFact = React.createFactory(MyTitle) // never have to use createFactory with JSX
 var ce = React.createElement
 
 var MyFirstComponent = (
 	div(null,
-		MyTitleFact({title: 'Props are great.'}),
-		React.createElement(MyTitle, {title: 'Use props everywhere.'}),
-		ce(MyTitle, {title: 'Props are the best.'})
+		MyTitleFact({title: 'Props are great.', color: 'rebeccapurple'}),
+		MyTitleFact({title: 'Use props, they\'re great!.', color: 'darkorange'}),
+		React.createElement(MyTitle, {title: 'Use props everywhere.', color: 'steelblue'}),
+		ce(MyTitle, {title: 'Props are the best.', color: 'slategrey'})
 	)
-)
+);
 
 ReactDOM.render(
 	MyFirstComponent, document.getElementById('app')
-)
+);
