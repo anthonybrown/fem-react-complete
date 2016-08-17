cd51e6fe39caaaf43034e1c7187c87c10706db1d
Tony Brown <tony@tonybrown.me>
2016-08-17 19:40:46 -0400

    using require, using webpack and npm scripts

--- js/clientApp.js
+++ js/clientApp.js
@@ -1,30 +1,19 @@
-'use strict';
-
-var div = React.DOM.div;
-var h1  = React.DOM.h1;
-
-var MyTitle = React.createClass({
-	render () {
-		return (
-			div(null,
-				h1({style: {color: this.props.color}}, this.props.title)
-			)
-		);
-	}
-});
+'use strict'
+var React = require('react')
+var ReactDOM = require('react-dom')
+var MyTitle	= require('./MyTitle')
+var	div	= React.DOM.div
 
 var MyTitleFact = React.createFactory(MyTitle) // never have to use createFactory with JSX
-var ce = React.createElement
+var ce					= React.createElement
 
 var MyFirstComponent = (
 	div(null,
-		MyTitleFact({title: 'Props are great.',                       color : 'rebeccapurple'}),
-		MyTitleFact({title: 'Use props, they\'re great!.',            color : 'darkorange'}),
-		React.createElement(MyTitle, {title: 'Use props everywhere.', color : 'steelblue'}),
-		ce(MyTitle, {title: 'Props are the best.',                    color : 'slategrey'})
+		MyTitleFact({title: 'Props are great.', color: 'rebeccapurple'}),
+		MyTitleFact({title: 'Use props, they\'re great!.', color: 'mediumaquamarine'}),
+		React.createElement(MyTitle, {title: 'Use props everywhere.', color: 'steelblue'}),
+		ce(MyTitle, {title: 'Props are the best.', color: 'slategrey'})
 	)
-);
+)
 
-ReactDOM.render(
-	MyFirstComponent, document.getElementById('app')
-);
+ReactDOM.render(MyFirstComponent, document.getElementById('app'))
